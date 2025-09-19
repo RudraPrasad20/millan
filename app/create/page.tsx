5ffee4416c1b5cd0c3a2b629be89a23627a13c81
Rudra Prasad <rudraofficial202@gmail.com>
2025-09-19 11:16:12 +0530

    adding create form

---
+++ app/create/page.tsx
@@ -0,0 +1,14 @@
+// create list
+
+import { CreateForm } from '@/components/createForm'
+import React from 'react'
+
+const CreatePage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <CreateForm/>
+    </div>
+  )
+}
+
+export default CreatePage
---
+++ components/createForm.tsx
@@ -0,0 +1,153 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  email: z.email().min(5, {
+    message: "Email must be at least 2 characters.",
+  }),
+  walletAddress: z.base64().min(44, {
+    message: "Enter a valid Wallet Address.",
+  }),
+  amount: z.string().min(2, {
+    message: "Amount.",
+  }),
+  details: z.string().optional(),
+});
+
+export function CreateForm() {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      walletAddress: "",
+      amount: "",
+      details: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Create Entity</CardTitle>
+        <CardDescription>Fill the form to Create new Entity</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Full Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Wallt" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email Address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Walltjr30@gmail.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="walletAddress"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Wallet Address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="G38n3jdu3..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Enter Amount</FormLabel>
+                  <FormControl>
+                    <Input placeholder="0.02" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="details"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input placeholder="For Bulk Deal" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <CardFooter className="flex flex-col sm:flex-row gap-2 justify-center items-center">
+              <Button className="cursor-pointer" type="submit">
+                Submit
+              </Button>
+              <Button
+                variant={"secondary"}
+                className="cursor-pointer"
+                onClick={() => router.back()}
+              >
+                Back
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
